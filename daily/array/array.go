8c2d4c79bcd2bb00fc97c8a165623af47bc85335
Song <1335130740@qq.com>
2023-10-10 21:57:20 +0800

    pass 16. 3Sum Closest(middle array)

---
+++ daily/array/array.go
@@ -0,0 +1,72 @@
+package array
+
+import (
+	"math"
+	"sort"
+)
+
+/*
+ThreeSumClosest16
+16. 3Sum Closest
+find three num in nums which sum close to target
+*/
+func ThreeSumClosest16(nums []int, target int) int {
+	n := len(nums)
+	// first, sort nums in increasing order
+	sort.Ints(nums)
+
+	closest := math.MaxInt32
+
+	update := func(sum int) {
+		if abs(sum-target) < abs(closest-target) {
+			closest = sum
+		}
+	}
+
+	// throughout first num
+	for i := 0; i < n; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		a := nums[i]
+
+		// throughout all b, c in nums[i+1, n-1]
+		// find b, c from left and right: [i+1, n-1]
+		for j, k := i+1, n-1; j < k; {
+			b := nums[j]
+			c := nums[k]
+
+			sum := a + b + c
+			if sum == target {
+				return target
+			}
+			// try update
+			update(sum)
+			// scale range about b and c
+			if sum < target {
+				// b is not considered anymore because no third number(using this b) can meet their sum closer to target
+				// move b to right to enlarge b so that enlarge sum
+				for j+1 < k && nums[j+1] == nums[j] {
+					j++
+				}
+				j++
+			} else { // a + b + c > target
+				// c is not considered anymore because no second number(using this c) can meet their sum closer to target
+				// move c to left to reduce c so that reduce sum
+				for k-1 > j && nums[k-1] == nums[k] {
+					k--
+				}
+				k--
+			}
+		}
+	}
+	return closest
+
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
---
+++ daily/array/array_test.go
@@ -0,0 +1,25 @@
+package array
+
+import "testing"
+
+func TestThreeSumClosest16(t *testing.T) {
+	cases := []struct {
+		nums   []int
+		target int
+		ans    int
+	}{
+		{
+			nums:   []int{1, 1, 1, 0},
+			target: -100,
+			ans:    2,
+		},
+	}
+
+	for _, cas := range cases {
+		res := ThreeSumClosest16(cas.nums, cas.target)
+		if res != cas.ans {
+			t.Fatalf("expected %d, but get %d", cas.ans, res)
+		}
+	}
+
+}
